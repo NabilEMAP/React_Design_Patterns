32fbfdfea11aeba419d7e77721a86c2c61287da4
NabilEMAP <Nabil_EM@outlook.com>
2023-09-02 19:06:56 +0200

    New CreateReactApp Slate with SplitScreen.js

--- README.md
+++
@@ -1,4 +0,0 @@
-# React Design Patterns
-
-[Learning with Shaun Wassell](https://www.linkedin.com/learning/react-design-patterns/take-your-react-skills-to-the-next-level?contextUrn=urn%3Ali%3AlyndaLearningPath%3A5b32b6d5498e4ef39c04c55c&resume=false&u=93921834)
-
--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,13 +1,10 @@
 import { SplitScreen } from './SplitScreen';
-
 const LeftHandComponent = () => {
   return <h1 style={{backgroundColor: 'red'}}>Left!</h1>
 }
-
 const RightHandComponent = () => {
   return <h1 style={{backgroundColor: 'green'}}>Right!</h1>
 }
-
 function App() {
   return (
     <SplitScreen
@@ -16,5 +13,4 @@ function App() {
     />
   );
 }
-
 export default App;
