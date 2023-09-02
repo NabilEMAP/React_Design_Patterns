e34e6ea186a703ce3050c2816f575f3a9244c4b6
NabilEMAP <Nabil_EM@outlook.com>
2023-09-02 19:25:51 +0200

    Split-screen component improvements 2

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,18 +1,19 @@
 import { SplitScreen } from './SplitScreen';
 const LeftHandComponent = () => {
-  return <h1 style={{backgroundColor: 'red'}}>Left!</h1>
+  return <h1 style={{ backgroundColor: 'red' }}>Left!</h1>
 }
 const RightHandComponent = () => {
-  return <h1 style={{backgroundColor: 'green'}}>Right!</h1>
+  return <h1 style={{ backgroundColor: 'green' }}>Right!</h1>
 }
 function App() {
   return (
     <SplitScreen
-      left={LeftHandComponent}
-      right={RightHandComponent}
       leftWeight={1}
       rightWeight={3}
-    />
+    >
+      <LeftHandComponent />
+      <RightHandComponent />
+    </SplitScreen>
   );
 }
 export default App;
--- react-design-patterns/src/SplitScreen.js
+++ react-design-patterns/src/SplitScreen.js
@@ -6,18 +6,18 @@ const Pane = styled.div`
     flex: ${props => props.weight};
 `;
 export const SplitScreen = ({
-    left: Left,
-    right: Right,
+    children,
     leftWeight = 1,
     rightWeight = 1,
 }) => {
+    const [left, right] = children;
     return (
         <Container>
             <Pane weight={leftWeight}>
-                <Left />
+                {left}
             </Pane>
             <Pane weight={rightWeight}>
-                <Right />
+                {right}
             </Pane>
         </Container>
     );
