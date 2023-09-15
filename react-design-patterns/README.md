a26e46edc0cada135f33128dc6499838e9ae5a7d
NabilEMAP <Nabil_EM@outlook.com>
2023-09-15 02:03:20 +0200

    useDataSource Hook

--- react-design-patterns/README.md
+++ react-design-patterns/README.md
@@ -45,3 +45,4 @@
 - useCurrentUser Hook
 - useUser Hook
 - useResource Hook
+- useDataSource Hook
--- react-design-patterns/src/UserInfo.js
+++ react-design-patterns/src/UserInfo.js
@@ -1,9 +1,23 @@
-import { useResource } from "./useResource";
+import { useDataSource } from "./useDataSource";
+import axios from 'axios';
+
+const serverResource = resourceUrl => async () => {
+	const response = await axios.get(resourceUrl);
+	return response.data;
+}
+
+/*
+const localStorageResource = key => {
+	return localStorage.getItem(key);
+}
+*/
 
 export const UserInfo = ({ userId }) => {
-	const user = useResource(`/users/${userId}`);
+	// const user = useResource(`/users/${userId}`);
+	const user = useDataSource(serverResource(`/users/${userId}`));
+	// const message = useDataSource(localStorageResource('message'))
+	
 	const { name, age, hairColor, hobbies } = user || {};
-
 	return user ? (
 		<>
 			<h3>{name}</h3>
